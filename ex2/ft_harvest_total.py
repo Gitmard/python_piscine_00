a7cc01117ef588a52f30092e21baf4ae90b79204
smenard <floogles71@gmail.com>
2026-02-02 14:24:36 +0100

    refacto: simplify harverst_total

--- ex2/ft_harvest_total.py
+++ ex2/ft_harvest_total.py
@@ -1,8 +1,5 @@
 def ft_harvest_total():
-    harvests: list = []
     total: int = 0
     for i in range(3):
-        harvests.append(int(input(f"Day {i+1} harvest: ")))
-    for day in harvests:
-        total += day
+        total += int(input(f"Day {i+1} harvest: "))
     print(f"Total harvest: {total}")
